7b6a27359695068c78f99451abd209cda9d0d5ea
SEONHU KANG <104255115+SXXNHU@users.noreply.github.com>
2025-05-29 17:01:57 +0900

    Delete BE_pangjam_homework directory

--- BE_pangjam_homework/src/main/java/like_lion/pangjam/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package like_lion.pangjam.Repository;
-
-import like_lion.pangjam.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
