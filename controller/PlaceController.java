494e86023a36e4ef2a08ecb476f048498088a360
SEONHU KANG <104255115+SXXNHU@users.noreply.github.com>
2025-05-29 17:38:48 +0900

    Update PlaceController.java

--- controller/PlaceController.java
+++ controller/PlaceController.java
@@ -4,9 +4,7 @@ import like_lion.pangjam.dto.global.SliceResponseDto;
 import like_lion.pangjam.dto.place.PlaceResponseDto;
 import like_lion.pangjam.service.PlaceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,22 +17,15 @@ public class PlaceController {
     private final PlaceService placeService;
 
     // place 조회
-    @GetMapping
-    public ResponseEntity<SliceResponseDto<PlaceResponseDto.PlaceInfo>> getPlaces(
+    @GetMapping("page/{page}")
+    public ResponseEntity<SliceResponseDto<PlaceResponseDto.PlaceInfo>> searchPlacesInfos(
+            @PathVariable int page,
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String category,
-            @PageableDefault(size = 15) Pageable pageable
+            @RequestParam(defaultValue = "15") int size
     ) {
-        Slice<PlaceResponseDto.PlaceInfo> slice = placeService.getPlaces(name, category, pageable);
-
-        SliceResponseDto<PlaceResponseDto.PlaceInfo> response = SliceResponseDto.of(
-                slice.getContent(),
-                slice.hasNext(),
-                pageable.getPageNumber(),
-                pageable.getPageSize()
-        );
-
-        return ResponseEntity.ok(response);
+        SliceResponseDto <PlaceResponseDto.PlaceInfo> placeSlice = placeService.searchPlacesInfos(name, category, size, page);
+        return ResponseEntity.ok(placeSlice);
     }
 
     // place 상세 조회
