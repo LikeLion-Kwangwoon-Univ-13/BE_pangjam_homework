416182762dab4d5273f691c6e8b2e4374217275f
SEONHU KANG <104255115+SXXNHU@users.noreply.github.com>
2025-05-29 17:00:13 +0900

    Delete pangjam directory

--- pangjam/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package like_lion.pangjam.repository;
-
-import like_lion.pangjam.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
